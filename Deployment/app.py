5276ce9468e77d3343a7518c8a454c867924916f
keziahutagaol <112603526+keziahutagaol@users.noreply.github.com>
2025-05-14 15:26:07 +0700

    Update ipynb

--- Deployment/app.py
+++ Deployment/app.py
@@ -65,4 +65,5 @@ def predict():
         return render_template('index.html', prediction_text=f"Terjadi kesalahan: {str(e)}")
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port=5001)
+
